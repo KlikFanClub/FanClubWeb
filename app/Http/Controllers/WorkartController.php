07b1a2a71efa5639a003fabad534870c29c04020
Gabi Piñeiro <gabispineiro@gmail.com>
2021-08-18 09:24:38 +0200

    creacion de la estructura laravel de las tablas

---
+++ app/Http/Controllers/WorkartController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\workart;
+use Illuminate\Http\Request;
+
+class WorkartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\workart  $workart
+     * @return \Illuminate\Http\Response
+     */
+    public function show(workart $workart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\workart  $workart
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(workart $workart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\workart  $workart
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, workart $workart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\workart  $workart
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(workart $workart)
+    {
+        //
+    }
+}
