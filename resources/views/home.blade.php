bcca99a314c2bb0bb6bd40d8467316665111e781
martindejos <martindejos@gmail.com>
2021-08-18 10:56:15 +0200

    header & footer component in welcome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -18,6 +19,9 @@
                 </div>
             </div>
         </div>
+        
     </div>
+    
 </div>
+
 @endsection
