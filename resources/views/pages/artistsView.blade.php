b82ba95fcb4f84f15756406adb434b75867efbe3
martindejos <martindejos@gmail.com>
2021-08-19 12:28:01 +0200

    footer

--- resources/views/pages/artistsView.blade.php
+++ resources/views/pages/artistsView.blade.php
@@ -1,8 +1,10 @@
 @extends('welcome')
 @section('content')
 
-    <div>
-        ARTISTAS
+    <div class="d-flex text-success">
+        <p>ARTISTAS</p>
+        <p>ARTISTAS</p>
+        <p>ARTISTAS</p>
     </div>
 
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,22 +22,25 @@
     </head>
     <body class="antialiased">
         <x-header/>
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden   right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+        
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <x-footer/>
+        <x-footer/>
         </div>
     </body>
 </html>
+
+
+{{-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    @if (Route::has('login'))
+        <div class="hidden   right-0 px-6 py-4 sm:block">
+            @auth
+                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+            @else
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Logssss in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif --}}
