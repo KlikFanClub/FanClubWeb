fd321c7f533bba945f58d742b12ad003021ea97d
Davidhdm <david.hdm9@gmail.com>
2021-08-25 12:18:08 +0200

    catalogue artwork cards grid

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,31 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" >
-    <script src="https://kit.fontawesome.com/589f153f6d.js" crossorigin="anonymous"></script>
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/589f153f6d.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-
-    <div id="app">
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-
+  <div id="app">
+    @yield('content')
+  </div>
 </body>
+
 </html>
--- resources/views/pages/workarts.blade.php
+++ resources/views/pages/workarts.blade.php
@@ -1,21 +1,39 @@
 @extends ('layouts.app')
-@section ('content')
+@section('content')
 
-    <x-header />
+  <x-header />
 
-@foreach ($workarts as $workart)
-<img src="{{ $workart->imageworkart }}" alt="">
-<h5>{{ $workart->title . ' - ' . $workart->created_at }}</h5>
-<h3>{{ $workart->artistname }}</h3>
-<h5>{{ $workart->price }}</h5>
-<div>
-    <a href="{{route ('editWorkart', ['id'=>$workart->id])}}">
-        <i class="far fa-edit"></i>
-    </a>
-   <a href="">
-    <i class="fas fa-trash-alt"></i>
-   </a>
-</div>
-@endforeach
+  <div class="catalogue">
+    <div class="mobile_filterBtn"></div>
+    <aside class="filter">
+      <span class="filter_category">TODOS LOS PRODUCTOS</span>
+      <span class="filter_category">EDICIÓN</span>
+      <span class="filter_category">TAMAÑO</span>
+      <span class="filter_category">PRECIO</span>
+      <span class="filter_category">TÉCNICA</span>
+      <span class="filter_category">TEMAS</span>
+      <span class="filter_category">OTROS</span>
+      <span class="filter_category">ARTISTAS</span>
+    </aside>
 
+    <main class="catalogue_artworks">
+      @foreach ($workarts as $workart)
+        <div class="artwork_card">
+          <img class="artwork_img" src="{{ $workart->imageworkart }}" alt="">
+          <span class="artwork_title">{{-- {{ $workart->title . ' - ' . $workart->created_at }} --}}Nombre de la obra</span>
+          <span class="artwork_artistName">{{-- {{ $workart->artistname }} --}}Nombre del artista</span>
+          <span class="artwork_date">10/12/2016</span>
+          <span class="artwork_price">{{-- {{ $workart->price }} --}}123€</span>
+          <div class="action_buttons">
+            <a href="{{ route('editWorkart', ['id' => $workart->id]) }}">
+              <i class="far fa-edit"></i>
+            </a>
+            <a href="">
+              <i class="fas fa-trash-alt"></i>
+            </a>
+          </div>
+        </div>
+      @endforeach
+    </main>
+  </div>
 @endsection
