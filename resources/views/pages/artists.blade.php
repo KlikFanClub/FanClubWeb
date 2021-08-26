4786fc0230880ce9576db30254f3560c3026dca7
Gabi Piñeiro <gabispineiro@gmail.com>
2021-08-26 09:31:19 +0200

    Attempt to authenticate the administrator. Many mistakes. I will make a new branch and start again

--- resources/views/pages/artists.blade.php
+++ resources/views/pages/artists.blade.php
@@ -4,16 +4,26 @@
     <x-header />
 
 @foreach ($artists as $artist)
-    <img src="{{ $artist->profile_picture }}">
-    <h6>{{ $artist->name }}</h6>
-    <div>
-        <a href="{{route ('editArtist', ['id'=>$artist->id])}}">
-            <i class="far fa-edit"></i>
-        </a>
-        <a href="{{route ('deleteArtist', ['id'=>$artist->id])}}">
-            <i class="fas fa-trash-alt"></i>
-        </a>
-    </div>
+    
+    @auth
+       @if (Auth::user()->isAdmin){
+        <img src="{{ $artist->profile_picture }}">
+        <h6>{{ $artist->name }}</h6>
+        <div>
+            <a href="{{route ('editArtist', ['id'=>$artist->id])}}">
+                <i class="far fa-edit"></i>
+            </a>
+            <a href="{{route ('deleteArtist', ['id'=>$artist->id])}}">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </div>
+       } 
+       <img src="{{ $artist->profile_picture }}">
+       <h6>{{ $artist->name }}</h6>
+       @endif    
+
+    @endauth
+    
 @endforeach
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->middleware('authAdmin')->name('home');
 
 //Routes Workart
 
