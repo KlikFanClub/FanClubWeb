66ee271aeceafc9018f35cb8dec39e08b28685dd
Gabi Piñeiro <gabispineiro@gmail.com>
2021-08-26 13:01:54 +0200

    fixed auth admin, auth no admin and no auth. All finalized

--- resources/views/pages/artists.blade.php
+++ resources/views/pages/artists.blade.php
@@ -1,24 +1,22 @@
 @extends ('layouts.app')
-@section ('content')
+@section('content')
 
     <x-header />
 
-@foreach ($artists as $artist)   
+    @foreach ($artists as $artist)
         <img src="{{ $artist->profile_picture }}">
         <h6>{{ $artist->name }}</h6>
-     @if (Auth::user()->isAdmin)
-        <div>
-            <a href="{{route ('editArtist', ['id'=>$artist->id])}}">
-                <i class="far fa-edit"></i>
-            </a>
-            <a href="{{route ('deleteArtist', ['id'=>$artist->id])}}">
-                <i class="fas fa-trash-alt"></i>
-            </a>
-        </div>
-    @else
-    <img src="{{ $artist->profile_picture }}">
-    <h6>{{ $artist->name }}</h6>
-    @endif
-@endforeach
-
+        @if (Auth::user())
+            @if (Auth::user()->isAdmin)
+                <div>
+                    <a href="{{ route('editArtist', ['id' => $artist->id]) }}">
+                        <i class="far fa-edit"></i>
+                    </a>
+                    <a href="{{ route('deleteArtist', ['id' => $artist->id]) }}">
+                        <i class="fas fa-trash-alt"></i>
+                    </a>
+                </div>
+            @endif
+        @endif
+    @endforeach
 @endsection
--- resources/views/pages/workarts.blade.php
+++ resources/views/pages/workarts.blade.php
@@ -1,28 +1,24 @@
 @extends ('layouts.app')
-@section ('content')
+@section('content')
 
     <x-header />
 
-@foreach ($workarts as $workart)
-    <img src="{{ $workart->imageworkart }}" alt="">
-    <h5>{{ $workart->title . ' - ' . $workart->created_at }}</h5>
-    <h3>{{ $workart->artistname }}</h3>
-    <h5>{{ $workart->price }}</h5>
-@if (Auth::user()->isAdmin)
-    <div>
-        <a href="{{route ('editWorkart', ['id'=>$workart->id])}}">
-            <i class="far fa-edit"></i>
-        </a>
-    <a href="{{route ('deleteWorkart', ['id'=>$workart->id])}}">
-        <i class="fas fa-trash-alt"></i>
-    </a>
-    </div>
-@else
-    <img src="{{ $workart->imageworkart }}" alt="">
-    <h5>{{ $workart->title . ' - ' . $workart->created_at }}</h5>
-    <h3>{{ $workart->artistname }}</h3>
-    <h5>{{ $workart->price }}</h5>
-@endif
-@endforeach
-
+    @foreach ($workarts as $workart)
+        <img src="{{ $workart->imageworkart }}" alt="">
+        <h5>{{ $workart->title . ' - ' . $workart->created_at }}</h5>
+        <h3>{{ $workart->artistname }}</h3>
+        <h5>{{ $workart->price }}</h5>
+        @if (Auth::user())
+            @if (Auth::user()->isAdmin)
+                <div>
+                    <a href="{{ route('editWorkart', ['id' => $workart->id]) }}">
+                        <i class="far fa-edit"></i>
+                    </a>
+                    <a href="{{ route('deleteWorkart', ['id' => $workart->id]) }}">
+                        <i class="fas fa-trash-alt"></i>
+                    </a>
+                </div>
+            @endif
+        @endif
+    @endforeach
 @endsection
