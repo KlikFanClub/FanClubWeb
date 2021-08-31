a76cc6d9b70c7d5d7507955fefdac2d6e0c197ed
Joan Recordà Pellicer <recorda@gmail.com>
2021-08-31 15:56:37 +0200

    actualization

--- resources/views/artist/create.blade.php
+++ resources/views/artist/create.blade.php
@@ -6,30 +6,17 @@
 
     <div class="form-container">
 
-        <form method="POST" action="{{route ('storeArtists')}}">
+        <form method="POST" action="{{route ('storeArtist')}}">
             @csrf
 
             <div class="form-group row">
 
-                <h4>Formulario de la obra de arte</h4>
+                <h4>Formulario del artista</h4>
                 <br>
-                <h6>Introduzca a continuación los datos de la obra</h6>
+                <h6>Introduzca a continuación los datos del artista</h6>
                 <br>
 
                 <div class="form-box-container">
-                    <div class="row">
-                        <div class="col-25">
-                            <label for="artist_id">Artista</label>
-                        </div>
-                        <div class="col-75">
-                            <select name="artist_id" >
-                                <option value="" selected disabled>Seleccione un artista</option>
-                                @foreach ($artists as $artist)
-                                    <option value="{{$artist->id}}">{{$artist->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
 
                     <div class="row">
                         <div class="col-25">
