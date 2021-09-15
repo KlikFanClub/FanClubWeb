9c3aaa02b9ae42b37874f135d728c31d1a6107b1
Joan Recordà Pellicer <recorda@gmail.com>
2021-09-15 14:24:41 +0200

    actualization

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -66,7 +66,6 @@
       <!-- Copyright -->
 
         @guest
-
             @if (Route::has('login'))
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -83,7 +82,6 @@
                 <a id="dropdownMenuButton" class="nav-link dropdown-toggle" href="#" role="button"
                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                     {{ Auth::user()->name }}
-
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
