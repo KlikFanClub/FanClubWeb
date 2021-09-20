0a3046f8cdea7dcd7d1b1a4a5944dc7dae043a69
Andrea Suarez <asuareztamayo@hotmail.com>
2021-09-20 09:12:42 +0200

    ocultar register

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -73,7 +73,7 @@
                 </li>
             @endif
             @if (Route::has('register'))
-                <li class="nav-item">
+                <li class="nav-item d-none">
                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                 </li>
             @endif
