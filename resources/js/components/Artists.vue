be2bc5a0274a93e0297f8024f157b40253cf554e
Davidhdm <david.hdm9@gmail.com>
2021-09-22 23:47:39 +0200

    created Artists vue component

---
+++ resources/js/components/Artists.vue
@@ -0,0 +1,24 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'Artists',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  created: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
