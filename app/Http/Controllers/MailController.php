1aa9014df75fa31a62b91cba2b48e9caacf77f40
Gabi Piñeiro <gabispineiro@gmail.com>
2021-09-23 12:09:59 +0200

    pull dev

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -12,22 +12,5 @@ class MailController extends Controller
     public function index()
     {
         return view('emails.mail');
-    }
-
-   /*  public function store(Request $request)
-    {
-        $contact=([
-            'name' => $request['name'],
-            'email' => $request['email'],            
-            'aboutArtist' => $request['aboutArtist'],
-            'someLink' => $request['someLink'],
-        ]);
-
-        $correo = new New_Artist_Mail($contact);
-      
-
-        Mail::to('gabipineiro@hotmail.com')->send($correo);
-
-        return 'Gracias por estar con nosostros! Tu email ha sido enviado a FanClub y pronto te contestaremos!';
-    } */
+    }  
 }
