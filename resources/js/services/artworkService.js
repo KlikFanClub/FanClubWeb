ef084fd213c08cdb694c0dcaf546afc6bd5f9cd6
Davidhdm <david.hdm9@gmail.com>
2021-09-26 11:44:47 +0200

    updated vue api service base url

--- resources/js/services/artworkService.js
+++ resources/js/services/artworkService.js
@@ -4,9 +4,9 @@ const baseURL = 'http://fanclubweb.herokuapp.com/'
 
 export const artworkService = {
   getAllArtworks() {
-    return axios.get(baseURL + 'api/workarts')
+    return axios.get('http://fanclubweb.herokuapp.com/api/workarts')
   },
   getArtwork(id) {
-    return axios.get(baseURL + 'api/workarts/' + id)
+    return axios.get('http://fanclubweb.herokuapp.com/api/workarts/' + id)
   }
 }
